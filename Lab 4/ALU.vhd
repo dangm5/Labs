6e94b4fb305a088695d4bd5ed3eae554516b22cb
dangm5 <38116768+dangm5@users.noreply.github.com>
2018-05-14 15:04:27 -0700

    Update ALU.vhd

--- Lab 4/ALU.vhd
+++ Lab 4/ALU.vhd
@@ -4,10 +4,10 @@
 --
 --------------------------------------------------------------------------------
 
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
-USE IEEE.std_logic_unsigned.all;
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
 
 entity fulladder is
     port (a : in std_logic;
@@ -19,9 +19,9 @@ entity fulladder is
 end fulladder;
 
 architecture addlike of fulladder is
-	begin
-  		sum   <= a xor b xor cin; 
-  		carry <= (a and b) or (a and cin) or (b and cin); 
+begin
+  sum   <= a xor b xor cin; 
+  carry <= (a and b) or (a and cin) or (b and cin); 
 end architecture addlike;
 
 --------------------------------------------------------------------------------
@@ -32,33 +32,33 @@ Use ieee.numeric_std.all;
 Use ieee.std_logic_unsigned.all;
 
 entity adder_subtracter is
-	port( datain_a: in std_logic_vector(31 downto 0);
-		datain_b: in std_logic_vector(31 downto 0);
-		add_sub: in std_logic;
-		dataout: out std_logic_vector(31 downto 0);
-		co: out std_logic);
+port( datain_a: in std_logic_vector(31 downto 0);
+datain_b: in std_logic_vector(31 downto 0);
+add_sub: in std_logic;
+dataout: out std_logic_vector(31 downto 0);
+co: out std_logic);
 end entity adder_subtracter;
 
 architecture calc of adder_subtracter is
 
-	component fulladder is    -- FullAdder Component Declaration
-		port (a : in std_logic;
+component fulladder is    -- FullAdder Component Declaration
+port (a : in std_logic;
                 b : in std_logic;
                 cin : in std_logic;
                 sum : out std_logic;
                 carry : out std_logic);
-	end component;
+end component;
  
 signal c: std_logic_vector (32 downto 0);
 signal sub: std_logic_vector (31 downto 0);
 
 begin
-	with add_sub select 
-		sub <= not (datain_b) when '1',
-		datain_b when others;
+with add_sub select 
+sub <= not (datain_b) when '1',
+datain_b when others;
  
-	c(0) <= add_sub;
-	co <= c(32);
+c(0) <= add_sub;
+co <= c(32);
 
 FullAdder32: for i in 0 to 31 generate
 FAi: FullAdder port map (datain_a(i), sub(i),c(i),dataout(i), c(i + 1));
@@ -76,23 +76,24 @@ Use ieee.numeric_std.all;
 Use ieee.std_logic_unsigned.all;
 
 entity shift_register is
-	port( datain: in std_logic_vector(31 downto 0);
-    		dir: in std_logic;
-		shamt: in std_logic_vector(4 downto 0);
+	port(	datain: in std_logic_vector(31 downto 0);
+	   	dir: in std_logic;
+		shamt:	in std_logic_vector(4 downto 0);
 		dataout: out std_logic_vector(31 downto 0));
 end entity shift_register;
 
 architecture shifter of shift_register is
+	
 begin
--- insert code here.
-	with dir & shamt select
-		dataout <= datain(30 downto 0) & '0' when "000001", 
+	-- insert code here.
+with dir & shamt select
+	dataout <= datain(30 downto 0) & '0' when "000001", 
 		datain(29 downto 0) & "00" when "000010", 
 		datain(28 downto 0) & "000" when "000011", 
 		'0' & datain(31 downto 1) when "100001", 
 		"00" & datain(31 downto 2) when "100010", 
 		"000" & datain(31 downto 3) when "100011",
-		datain(31 downto 0) when others;
+	datain(31 downto 0) when others;
 
 end architecture shifter;
 
@@ -104,7 +105,7 @@ Use ieee.numeric_std.all;
 Use ieee.std_logic_unsigned.all;
 
 entity ALU is
-	Port( DataIn1: in std_logic_vector(31 downto 0);
+	Port(	DataIn1: in std_logic_vector(31 downto 0);
 		DataIn2: in std_logic_vector(31 downto 0);
 		ALUCtrl: in std_logic_vector(4 downto 0);
 		Zero: out std_logic;
@@ -119,41 +120,43 @@ architecture ALU_Arch of ALU is
 	signal or_ins: std_logic_vector(31 downto 0);
 	signal result: std_logic_vector(31 downto 0);
 
--- ALU components
-component adder_subtracter
-	port( datain_a: in std_logic_vector(31 downto 0);
-		datain_b: in std_logic_vector(31 downto 0);
-		add_sub: in std_logic;
-		dataout: out std_logic_vector(31 downto 0);
-		co: out std_logic);
-end component adder_subtracter;
+	-- ALU components
+	
+	component adder_subtracter
+		port(	datain_a: in std_logic_vector(31 downto 0);
+			datain_b: in std_logic_vector(31 downto 0);
+			add_sub: in std_logic;
+			dataout: out std_logic_vector(31 downto 0);
+			co: out std_logic);
+	end component adder_subtracter;
 
-component shift_register
-	port( datain: in std_logic_vector(31 downto 0);
-    		dir: in std_logic;
-		shamt: in std_logic_vector(4 downto 0);
-		dataout: out std_logic_vector(31 downto 0));
-end component shift_register;
+	component shift_register
+		port(	datain: in std_logic_vector(31 downto 0);
+		   	dir: in std_logic;
+			shamt:	in std_logic_vector(4 downto 0);
+			dataout: out std_logic_vector(31 downto 0));
+	end component shift_register;
 
 begin
--- Add ALU VHDL implementation here
+	-- Add ALU VHDL implementation here
 
-	addsub: adder_subtracter port map(DataIn1, DataIn2, ALUCtrl(2), add_sub_ins, add_sub_co);
-	shift: shift_register port map(DataIn1, ALUCtrl(3), DataIn2(10 downto 6), shift_reg_ins);
+	addsub:	adder_subtracter port map(DataIn1, DataIn2, ALUCtrl(2), add_sub_ins, add_sub_co);
+	shift:	shift_register port map(DataIn1, ALUCtrl(3), DataIn2(10 downto 6), shift_reg_ins);
 
 	and_ins <= DataIn1 and DataIn2;
 	or_ins <= DataIn1 or DataIn2;
 
-with ALUCtrl(1 downto 0) select
-	result <= add_sub_ins when "00",
-  	shift_reg_ins when "01",
-  	and_ins when "10",
-  	or_ins when others;
+	with ALUCtrl(1 downto 0) select
+		result <= add_sub_ins when "00",
+			  shift_reg_ins when "01",
+			  and_ins when "10",
+			  or_ins when others;
 
-with result select
-	Zero <= '1' when "00000000000000000000000000000000",
-	'0' when others;
+	with result select
+		Zero <=	'1' when "00000000000000000000000000000000",
+			'0' when others;
 
 	ALUResult <= result;
 
-end architecture ALU_Arch;
+
+end architecture ALU_Arch; 
